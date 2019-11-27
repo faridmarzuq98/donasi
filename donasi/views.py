c480a0a869a89d9a313df02ec515dd0fe05fef5f
Muhammad Farid Marzuq <muhammad.faridmarzuq5@gmail.com>
2019-11-27 22:55:12 +0700

    dynamic navbar

--- donasi/views.py
+++ donasi/views.py
@@ -1,60 +1,59 @@
 from django.shortcuts import render
 from django.template import Template, Context
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, render
 from django.views import generic
 from .models import *
 
 # Create your views here.
 def index(request):
-    return render_to_response('usr/index.html')
+    return render(request, 'usr/index.html', {'nbar': 'home'})
 
 def about(request):
-    return render_to_response('usr/about.html')
+    return render(request, 'usr/about.html', {'nbar': 'about'})
 
 def campaign(request):
-    return render_to_response('usr/campaign.html')
+    return render(request, 'usr/campaign.html', {'nbar': 'campaign'})
 
 # @login_required
 def campaign(request, template_name='usr/campaign.html'):
     campaign = Campaign.objects.all()
     data = {}
     data['campaigns'] = campaign
-    return render(request, template_name, data)
+    return render(request, template_name, data, {'nbar': 'campaign'})
 
 def contact(request):
-    return render_to_response('usr/contact.html')
+    return render(request, 'usr/contact.html', {'nbar': 'contact'})
 
 def elements(request):
-    return render_to_response('usr/elements.html')
+    return render(request, 'usr/elements.html')
 
 def campaign_list(request):
-    return render_to_response('usr/campaign_list.html')
+    return render(request, 'usr/campaign_list.html', {'nbar': 'campaign'})
 
 def portfolio(request):
-    return render_to_response('usr/portfolio.html')
+    return render(request, 'usr/portfolio.html')
 
 def single_causes(request):
-    return render_to_response('usr/single-causes.html')
+    return render(request, 'usr/single-causes.html')
 
 def profile(request):
-    return render_to_response('usr/profile.html')
+    return render(request, 'usr/profile.html', {'nbar': 'profile'})
 
 #Admin side
 def adm(request):
-    return render_to_response('adm/index.html')
+    return render(request, 'adm/index.html')
 
 def error(request):
-    return render_to_response('adm/error-404.html')
+    return render(request, 'adm/error-404.html')
 
 def material(request):
-    return render_to_response('adm/icon-material.html')
+    return render(request, 'adm/icon-material.html')
 
 #def profile(request):
-#    return render_to_response('adm/pages-profile.html')
+#    return render(request, 'adm/pages-profile.html')
 
 def starter(request):
-    return render_to_response('adm/starter-kit.html')
+    return render(request, 'adm/starter-kit.html')
 
 def table(request):
-    return render_to_response('adm/table-basic.html')
+    return render(request, 'adm/table-basic.html')
